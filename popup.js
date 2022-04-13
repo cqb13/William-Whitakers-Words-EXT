4728be8bc1f3df6ab7b6ab6e229b39edc9cfa0cb
cqb13 <74616162+cqb13@users.noreply.github.com>
2022-04-13 14:24:44 -0400

    getting ready
    started work on google translate translation function

--- popup.js
+++ popup.js
@@ -1,11 +1,20 @@
 var buttonLTE = document.getElementById("LTE");
 var buttonETL = document.getElementById("ETL");
+const checkBox = document.querySelector('#box');
 var word = document.getElementById("entry");
 
 buttonLTE.addEventListener("click", function(){
-    chrome.tabs.create({url:"http://www.archives.nd.edu/cgi-bin/wordz.pl?keyword=" + word.value});
+    if (checkBox.checked == false) {
+        chrome.tabs.create({url:"http://www.archives.nd.edu/cgi-bin/wordz.pl?keyword=" + word.value});
+    } else {
+        console.log('google translate word')
+    }
 });
 
 buttonETL.addEventListener("click", function(){
-    chrome.tabs.create({url:"https://archives.nd.edu/cgi-bin/wordz.pl?english=" + word.value});
+    if (checkBox.checked == false) {
+        chrome.tabs.create({url:"https://archives.nd.edu/cgi-bin/wordz.pl?english=" + word.value});
+    } else{
+        console.log('google translate word')
+    }
 });
