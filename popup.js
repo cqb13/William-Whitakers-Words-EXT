f56849cc0acf324743f5bb66297835f46d5ee799
cqb13 <74616162+cqb13@users.noreply.github.com>
2022-04-14 11:50:15 -0400

    image name updates

--- popup.js
+++ popup.js
@@ -3,18 +3,41 @@ var buttonETL = document.getElementById("ETL");
 const checkBox = document.querySelector('#box');
 var word = document.getElementById("entry");
 
+const error = {
+    type: "basic",
+    iconUrl: "/images/icon128.png",
+    title: "error",
+    message: "text not found | enter a word",
+};
+
+const translation = {
+    type: "basic",
+    iconUrl: "/images/icon128.png",
+    title: "translation",
+    message: "(translation goes here)",
+    buttons: [
+        { title: 'Copy' },
+      ]
+};
+
 buttonLTE.addEventListener("click", function(){
-    if (checkBox.checked == false) {
+    if (word.value == ""){
+        chrome.notifications.create(error);
+    } else if (checkBox.checked == false) {
         chrome.tabs.create({url:"http://www.archives.nd.edu/cgi-bin/wordz.pl?keyword=" + word.value});
-    } else {
-        chrome.tabs.create({url:"https://translate.google.com/?sla=l&tl=en&text="+word.value+"&op=translate"});
+    } else { 
+        chrome.tabs.create({url:"https://translate.google.com/?sl=la&tl=en&text="+word.value+"&op=translate"});
+        chrome.notifications.create(translation); 
     }
 });
 
 buttonETL.addEventListener("click", function(){
-    if (checkBox.checked == false) {
+    if (word.value == ""){
+        chrome.notifications.create(error); 
+    } else if (checkBox.checked == false) {
         chrome.tabs.create({url:"https://archives.nd.edu/cgi-bin/wordz.pl?english=" + word.value});
     } else{
         chrome.tabs.create({url:"https://translate.google.com/?sl=en&tl=la&text="+word.value+"&op=translate"});
+        chrome.notifications.create(translation); 
     }
 });
