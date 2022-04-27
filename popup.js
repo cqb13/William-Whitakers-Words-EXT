347ecae2b32d6791aee65ef70a153429c883b247
cqb13 <74616162+cqb13@users.noreply.github.com>
2022-04-27 10:26:57 -0400

    error message updates

--- popup.js
+++ popup.js
@@ -10,14 +10,11 @@ const error = {
     message: "text not found | enter a word",
 };
 
-const translation = {
+const tooMany = {
     type: "basic",
     iconUrl: "/images/icon128.png",
-    title: "translation",
-    message: "(translation goes here)",
-    buttons: [
-        { title: 'Copy' },
-      ]
+    title: "error",
+    message: "only searches one word",
 };
 
 buttonLTE.addEventListener("click", function(){
@@ -35,9 +32,11 @@ buttonETL.addEventListener("click", function(){
     if (word.value == ""){
         chrome.notifications.create(error); 
     } else if (checkBox.checked == false) {
+        if ((word.value).indexOf(' ') >= 0){
+            chrome.notifications.create(tooMany);
+        }
         chrome.tabs.create({url:"https://archives.nd.edu/cgi-bin/wordz.pl?english=" + word.value});
     } else{
         chrome.tabs.create({url:"https://translate.google.com/?sl=en&tl=la&text="+word.value+"&op=translate"});
-        chrome.notifications.create(translation); 
     }
 });
