e7be51b5406131ac7d354095f7674eaa39bd3b19
cqb13 <74616162+cqb13@users.noreply.github.com>
2022-05-02 10:13:13 -0400

    more errors

--- popup.js
+++ popup.js
@@ -3,20 +3,43 @@ var buttonETL = document.getElementById("ETL");
 const checkBox = document.querySelector('#box');
 var word = document.getElementById("entry");
 
+// errors
 const error = {
     type: "basic",
     iconUrl: "/images/icon128.png",
     title: "error",
     message: "text not found | enter a word",
-};
+}
 
 const tooMany = {
     type: "basic",
     iconUrl: "/images/icon128.png",
     title: "error",
-    message: "only searches one word",
-};
+    message: "can only searches one word",
+}
 
+const translation = {
+    type: "basic",
+    iconUrl: "/images/icon128.png",
+    title: "translation",
+    message: 'translation',
+}
+
+// Warnings
+const boxCheck = {
+    type: "basic",
+    iconUrl: "/images/icon128.png",
+    title: "warning",
+    message: "this option is less acurate",
+}
+
+checkBox.addEventListener("change", function(){
+    if (this.checked){
+        chrome.notifications.create(boxCheck);
+    }
+});
+
+// Latin to English
 buttonLTE.addEventListener("click", function(){
     if (word.value == ""){
         chrome.notifications.create(error);
@@ -24,10 +47,10 @@ buttonLTE.addEventListener("click", function(){
         chrome.tabs.create({url:"http://www.archives.nd.edu/cgi-bin/wordz.pl?keyword=" + word.value});
     } else { 
         chrome.tabs.create({url:"https://translate.google.com/?sl=la&tl=en&text="+word.value+"&op=translate"});
-        chrome.notifications.create(translation); 
     }
 });
 
+// English to Latin
 buttonETL.addEventListener("click", function(){
     if (word.value == ""){
         chrome.notifications.create(error); 
