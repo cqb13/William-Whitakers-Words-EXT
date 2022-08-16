c3b3614c9e1b416a4f7bab29845a1d5e8143f6df
cqb13 <cqb13.main@gmail.com>
2022-08-15 22:30:21 -0400

    clean up

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 var buttonLTE = document.getElementById("LTE");
 var buttonETL = document.getElementById("ETL");
-const checkBox = document.querySelector('#box');
-var word = document.getElementById("entry");
+var word = document.getElementById("word");
 
 // errors
 const error = {
@@ -18,35 +17,12 @@ const tooMany = {
     message: "can only searches one word",
 }
 
-const translation = {
-    type: "basic",
-    iconUrl: "/images/icon128.png",
-    title: "translation",
-    message: 'translation',
-}
-
-// Warnings
-const boxCheck = {
-    type: "basic",
-    iconUrl: "/images/icon128.png",
-    title: "warning",
-    message: "this option is less acurate",
-}
-
-checkBox.addEventListener("change", function(){
-    if (this.checked){
-        chrome.notifications.create(boxCheck);
-    }
-});
-
 // Latin to English
 buttonLTE.addEventListener("click", function(){
     if (word.value == ""){
         chrome.notifications.create(error);
-    } else if (checkBox.checked == false) {
-        chrome.tabs.create({url:"http://www.archives.nd.edu/cgi-bin/wordz.pl?keyword=" + word.value});
     } else { 
-        chrome.tabs.create({url:"https://translate.google.com/?sl=la&tl=en&text="+word.value+"&op=translate"});
+        chrome.tabs.create({url:"https://archives.nd.edu/cgi-bin/wordz.pl?english=" + word.value});
     }
 });
 
@@ -54,12 +30,10 @@ buttonLTE.addEventListener("click", function(){
 buttonETL.addEventListener("click", function(){
     if (word.value == ""){
         chrome.notifications.create(error); 
-    } else if (checkBox.checked == false) {
-        if ((word.value).indexOf(' ') >= 0){
+    } else {
+        if ((word.value).indexOf(' ') >= 0) {
             chrome.notifications.create(tooMany);
         }
         chrome.tabs.create({url:"https://archives.nd.edu/cgi-bin/wordz.pl?english=" + word.value});
-    } else{
-        chrome.tabs.create({url:"https://translate.google.com/?sl=en&tl=la&text="+word.value+"&op=translate"});
     }
 });
