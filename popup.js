36de987c7caadf54b37f2bc736b32e6e6c3d0511
cqb13 <cqb13.main@gmail.com>
2022-09-02 11:58:31 -0400

    bad css is back!

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 const buttonLTE = document.getElementById("LTE");
 const buttonETL = document.getElementById("ETL");
+const viewMode = document.getElementById("view-mode");
 const word = document.getElementById("word");
+var lightMode = false;
 
 // errors
 const error = {
@@ -41,3 +43,23 @@ buttonETL.addEventListener("click", function () {
     });
   }
 });
+
+// Light / Dark mode
+viewMode.addEventListener("click", function () {
+  if (lightMode == false) {
+    lightMode = true;
+    viewMode.value = "Dark Mode";
+  } else {
+    lightMode = false;
+    viewMode.value = "Light Mode";
+  }
+  toggleViewMode();
+});
+
+function toggleViewMode() {
+  document.getElementsByTagName("body")[0].classList.toggle("light-mode");
+  buttonETL.classList.toggle("light-mode-border");
+  buttonLTE.classList.toggle("light-mode-border");
+  viewMode.classList.toggle("light-mode-border");
+  word.classList.toggle("light-mode-border");
+}
